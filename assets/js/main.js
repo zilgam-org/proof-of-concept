15724ffccec44695bcaa399f80edf2b4e44db38a
SHSR2001 <shyams.aat@gmail.com>
2021-08-09 13:35:35 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+import { BN, Long, bytes, units } from './node_modules/zilliqa-js/util'
+// import {BN, Long, bytes, units} from '@zilliqa-js/util';
+import { Zilliqa } from './node_modules/@zilliqa-js/zilliqa'
+// import Zilliqa from '@zilliqa-js/zilliqa';
+
+import { toBech32Address, getAddressFromPrivateKey } from './node_modules/@zilliqa-js/crypto'
+
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
