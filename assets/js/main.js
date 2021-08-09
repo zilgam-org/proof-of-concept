f19ae2b8c95731d97ccd7fbbdcfd784176093ffd
Madhumitha Balaji <65944319+Madhu-balaji-01@users.noreply.github.com>
2021-08-09 11:10:54 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,15 +28,15 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // /* SMART CONTRACT STUFF*/
 
-const {BN, Long, bytes, units} = require('@zilliqa-js/util');
-// import {BN, Long, bytes, units} from '@zilliqa-js/util';
-const {Zilliqa} = require('@zilliqa-js/zilliqa');
-// import Zilliqa from '@zilliqa-js/zilliqa';
+// const {BN, Long, bytes, units} = require('@zilliqa-js/util');
+// // import {BN, Long, bytes, units} from '@zilliqa-js/util';
+// const {Zilliqa} = require('@zilliqa-js/zilliqa');
+// // import Zilliqa from '@zilliqa-js/zilliqa';
 
-const {
-    toBech32Address,
-    getAddressFromPrivateKey,
-} = require('@zilliqa-js/crypto');
+// const {
+//     toBech32Address,
+//     getAddressFromPrivateKey,
+// } = require('@zilliqa-js/crypto');
 
 // import {
 //     toBech32Address,
